a4c2aa29acbf538e01ca462cda191667207620f9
hamid2006rv <hamid.ravaee@gmail.com>
2017-05-11 19:53:40 +0430

    ddd

--- db/website.sql
+++ db/website.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2017 at 04:40 PM
+-- Generation Time: May 11, 2017 at 05:23 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -40,14 +40,17 @@ CREATE TABLE IF NOT EXISTS `content` (
   `attachment3` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   `hasTitle` int(11) NOT NULL DEFAULT '1',
   UNIQUE KEY `cid` (`cid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `content`
 --
 
 INSERT INTO `content` (`cid`, `ctitle`, `cbody`, `orgid`, `uid`, `cdate`, `attachment1`, `attachment2`, `attachment3`, `hasTitle`) VALUES
-(4, '', '&lt;h1 style=&quot;text-align: center;&quot;&gt;&lt;span style=&quot;color: #ff0000;&quot;&gt;رشته مهندسی فن آوری اطلاعات&lt;/span&gt;&lt;/h1&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;&lt;img style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; title=&quot;مهندسی فن آوری&quot; src=&quot;http://www.beytoote.com/images/stories/scientific/sci210.jpg&quot; alt=&quot;مهندسی فن آوری&quot; /&gt;&lt;/p&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;در کشور ما نیز در راستای ایجاد تغییرات بنیادین و منطقی و دستیابی به نیروهای متخصص و کارآمد رشته فناوری اطلاعات به صورت متمرکز و غیرمتمرکز در مراکز آموزش عالی از جمله دانشگاه ها ارایه می شود.&lt;/p&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;رشته مهندسی فناوری اطلاعات، زیرمجموعه گروه فنی در دانشگاه محسوب می شود. این رشته دانشی را به مخاطب منتقل می کند که طی آن روش های جمع آوری اطلاعات، استفاده و انتقال آن میسر شود.&lt;/p&gt;\r\n&lt;div style=&quot;text-align: right;&quot;&gt;&amp;nbsp;مطالعه، طراحی، ساخت، راه اندازی، نگهداری سیستم های سخت افزاری و نرم افزاری، جمع آوری، سازمان دهی، طبقه بندی و انتقال اطلاعات مهم ترین اهدافی است که مهندسی فناوری اطلاعات آن را دنبال می کند. مهم ترین توانمندی دانش آموختگان این رشته، استفاده صحیح و بهره برداری به موقع از اطلاعات است. هسته اصلی این رشته را مهندسی کامپیوتر به ویژه گرایش نرم افزار تشکیل می دهد.&lt;/div&gt;', 2, NULL, '1396-02-21', NULL, NULL, NULL, 0);
+(4, '', '&lt;h1 style=&quot;text-align: center;&quot;&gt;&lt;span style=&quot;color: #ff0000;&quot;&gt;رشته مهندسی فن آوری اطلاعات&lt;/span&gt;&lt;/h1&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;&lt;img style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; title=&quot;مهندسی فن آوری&quot; src=&quot;http://www.beytoote.com/images/stories/scientific/sci210.jpg&quot; alt=&quot;مهندسی فن آوری&quot; /&gt;&lt;/p&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;در کشور ما نیز در راستای ایجاد تغییرات بنیادین و منطقی و دستیابی به نیروهای متخصص و کارآمد رشته فناوری اطلاعات به صورت متمرکز و غیرمتمرکز در مراکز آموزش عالی از جمله دانشگاه ها ارایه می شود.&lt;/p&gt;\r\n&lt;p style=&quot;text-align: right;&quot;&gt;رشته مهندسی فناوری اطلاعات، زیرمجموعه گروه فنی در دانشگاه محسوب می شود. این رشته دانشی را به مخاطب منتقل می کند که طی آن روش های جمع آوری اطلاعات، استفاده و انتقال آن میسر شود.&lt;/p&gt;\r\n&lt;div style=&quot;text-align: right;&quot;&gt;&amp;nbsp;مطالعه، طراحی، ساخت، راه اندازی، نگهداری سیستم های سخت افزاری و نرم افزاری، جمع آوری، سازمان دهی، طبقه بندی و انتقال اطلاعات مهم ترین اهدافی است که مهندسی فناوری اطلاعات آن را دنبال می کند. مهم ترین توانمندی دانش آموختگان این رشته، استفاده صحیح و بهره برداری به موقع از اطلاعات است. هسته اصلی این رشته را مهندسی کامپیوتر به ویژه گرایش نرم افزار تشکیل می دهد.&lt;/div&gt;', 2, NULL, '1396-02-21', NULL, NULL, NULL, 0),
+(5, '', '&lt;table dir=&quot;rtl&quot; width=&quot;100%&quot;&gt;\r\n&lt;tbody&gt;\r\n&lt;tr&gt;\r\n&lt;td&gt;\r\n&lt;h1&gt;&lt;span style=&quot;color: #000080;&quot;&gt;این صفحه در حال ساخت است&lt;/span&gt;&lt;/h1&gt;\r\n&lt;div&gt;\r\n&lt;p&gt;&lt;strong&gt;کاربر گرامی با تشکر از بازدیدتان، این صفحه در حال ساخت می باشد ، لطفأ بعدأ مراجعه فرمایید&lt;/strong&gt;&lt;/p&gt;\r\n&lt;/div&gt;\r\n&lt;/td&gt;\r\n&lt;/tr&gt;\r\n&lt;/tbody&gt;\r\n&lt;/table&gt;', 3, NULL, '1396-02-21', NULL, NULL, NULL, 0),
+(6, '', '&lt;table dir=&quot;rtl&quot; width=&quot;100%&quot;&gt;\r\n&lt;tbody&gt;\r\n&lt;tr&gt;\r\n&lt;td&gt;\r\n&lt;h1&gt;&lt;span style=&quot;color: #000080;&quot;&gt;این صفحه در حال ساخت است&lt;/span&gt;&lt;/h1&gt;\r\n&lt;div&gt;\r\n&lt;p&gt;&lt;strong&gt;کاربر گرامی با تشکر از بازدیدتان، این صفحه در حال ساخت می باشد ، لطفأ بعدأ مراجعه فرمایید&lt;/strong&gt;&lt;/p&gt;\r\n&lt;/div&gt;\r\n&lt;/td&gt;\r\n&lt;/tr&gt;\r\n&lt;/tbody&gt;\r\n&lt;/table&gt;', 4, NULL, '1396-02-21', NULL, NULL, NULL, 0),
+(7, '', '&lt;table dir=&quot;rtl&quot; width=&quot;100%&quot;&gt;\r\n&lt;tbody&gt;\r\n&lt;tr&gt;\r\n&lt;td&gt;\r\n&lt;h1&gt;&lt;span style=&quot;color: #000080;&quot;&gt;این صفحه در حال ساخت است&lt;/span&gt;&lt;/h1&gt;\r\n&lt;div&gt;\r\n&lt;p&gt;&lt;strong&gt;کاربر گرامی با تشکر از بازدیدتان، این صفحه در حال ساخت می باشد ، لطفأ بعدأ مراجعه فرمایید&lt;/strong&gt;&lt;/p&gt;\r\n&lt;/div&gt;\r\n&lt;/td&gt;\r\n&lt;/tr&gt;\r\n&lt;/tbody&gt;\r\n&lt;/table&gt;', 5, NULL, '1396-02-21', NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -88,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `organization` (
   `name` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   `top_id` int(11) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `organization`
@@ -96,7 +99,10 @@ CREATE TABLE IF NOT EXISTS `organization` (
 
 INSERT INTO `organization` (`id`, `name`, `top_id`) VALUES
 (1, 'آموزش', NULL),
-(2, 'IT', NULL);
+(2, 'IT', NULL),
+(3, 'امور دانشجویی', NULL),
+(4, 'امور پژوهشی', NULL),
+(5, 'تحصیلات تکمیلی', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
 --
 
 INSERT INTO `settings` (`num_visited`) VALUES
-(54);
+(67);
 
 -- --------------------------------------------------------
 
